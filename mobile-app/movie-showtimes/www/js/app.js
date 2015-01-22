40eca19fb5dfab7d743af65bbb0464f6c3f4997c
Dinh Quang Trung <trungdq88@gmail.com>
2015-01-22 11:56:29 +0700

    Add plugin

--- mobile-app/movie-showtimes/www/js/app.js
+++ mobile-app/movie-showtimes/www/js/app.js
@@ -3,7 +3,7 @@
     ons.ready(function() {
         // Hide Cordova splash screen when Onsen UI is loaded completely
         // API reference: https://github.com/apache/cordova-plugin-splashscreen/blob/master/doc/index.md
-        navigator.splashscreen.hide()
+        // navigator.splashscreen.hide()
     });
 
     var module = angular.module('app', ['onsen']);
