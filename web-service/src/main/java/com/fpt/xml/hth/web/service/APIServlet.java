081c95ee110d4d0d5fac64d3e63cd9810c43fc5b
Dinh Quang Trung <trungdq88@gmail.com>
2015-02-01 13:40:03 +0700

    Add sample data

--- web-service/src/main/java/com/fpt/xml/hth/web/service/APIServlet.java
+++ web-service/src/main/java/com/fpt/xml/hth/web/service/APIServlet.java
@@ -6,6 +6,7 @@ package com.fpt.xml.hth.web.service;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,14 +30,8 @@ public class APIServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("Hello, this is API");
-        } finally {            
-            out.close();
-        }
+
+        request.getRequestDispatcher("WEB-INF/sample.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
