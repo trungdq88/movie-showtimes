be11341acd8618dc34f33b75c159ebceddd8f04c
Dinh Quang Trung <trungdq88@gmail.com>
2015-02-08 21:37:42 +0700

    Caching xml by city

--- web-client/src/main/java/com/fpt/xml/hth/web/client/utils/EnvUtils.java
+++ web-client/src/main/java/com/fpt/xml/hth/web/client/utils/EnvUtils.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.fpt.xml.hth.web.client;
+package com.fpt.xml.hth.web.client.utils;
 
 import javax.servlet.ServletContext;
 
