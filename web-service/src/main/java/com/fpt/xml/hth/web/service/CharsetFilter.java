fe8644c9f78544c100cefe208f658cd49241c9bf
Dinh Quang Trung <trungdq88@gmail.com>
2015-02-08 14:31:12 +0700

    Fix servlet urf8 param

---
+++ web-service/src/main/java/com/fpt/xml/hth/web/service/CharsetFilter.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fpt.xml.hth.web.service;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+/**
+ *
+ * @author dinhquangtrung
+ */
+public class CharsetFilter implements Filter {
+
+    private String encoding;
+
+    public void init(FilterConfig config) throws ServletException {
+        encoding = config.getInitParameter("requestEncoding");
+
+        if (encoding == null) {
+            encoding = "UTF-8";
+        }
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain next)
+            throws IOException, ServletException {
+    // Respect the client-specified character encoding
+        // (see HTTP specification section 3.4.1)
+        if (null == request.getCharacterEncoding()) {
+            request.setCharacterEncoding(encoding);
+        }
+
+        /**
+         * Set the default response content type and encoding
+         */
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        next.doFilter(request, response);
+    }
+
+    public void destroy() {
+    }
+}
