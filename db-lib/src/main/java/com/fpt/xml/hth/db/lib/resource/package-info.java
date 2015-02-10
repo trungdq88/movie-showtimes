5b9ffa26f6d7072da468beddd284e79f996c2bf9
Nguyen Ngoc Thanh Hai <hainnt1302@gmail.com>
2015-02-10 11:08:04 +0700

    delete package-info

--- db-lib/src/main/java/com/fpt/xml/hth/db/lib/resource/package-info.java
+++
@@ -1,6 +0,0 @@
-@XmlSchema(
-        namespace="http://jbossews-trungdq88.rhcloud.com/API/movieSchema",
-        elementFormDefault=XmlNsForm.QUALIFIED)
-package com.fpt.xml.hth.db.lib.resource;
- 
-import javax.xml.bind.annotation.*;
